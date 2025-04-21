5d1e696f9f3e64051e89117530ab1f5cea203f9e
victorogwumike <143882557+victorogwumike@users.noreply.github.com>
2025-04-21 05:09:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-import os
 from fastapi import FastAPI
-import uvicorn
+from database import init_db
+import router.user as user_router
+import router.account as account_router
+import router.transaction as transaction_router
 
 app = FastAPI()
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+@app.on_event("startup")
+async def start_db():
+    await init_db()
+
+app.include_router(user_router.router)
+app.include_router(account_router.router)
+app.include_router(transaction_router.router)
+
+
